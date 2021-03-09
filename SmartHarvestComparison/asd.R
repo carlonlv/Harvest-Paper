d08dfe829f466a209f683a753bb4c0f41c16bb22
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-03-09 00:28:25 -0500

    Update asd.R

--- SmartHarvestComparison/asd.R
+++ SmartHarvestComparison/asd.R
@@ -14,9 +14,17 @@ window_type_for_reg <- c("max", "avg", "min", "sd", "median")
 granularity <- c(100 / 8, 100 / 16, 100 / 32, 100 / 64)
 
 cut_off_prob <- c(0.0001, 0.0003, 0.0005, 0.001, 0.003, 0.005, 0.01, 0.03, 0.05)
-additional_setting <- list("cut_off_prob" = cut_off_prob)
+additional_setting <- list("cut_off_prob" = cut_off_prob, "window_type_for_reg" = window_type_for_reg)
 
-bg_param_setting <- data.frame(class = "MULTINOM", name = "Multinomial", window_size = window_size, window_size_for_reg = window_size_for_reg, granularity = 0, train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE)
-d <- run_sim(bg_param_setting, additional_setting, microsoft_generated_data_2000, list("avg" = microsoft_generated_data_2000, "min"), cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/FindBestPredictor/Markov/")
+bg_param_setting <- expand.grid("granularity" = granularity, "window_size" = window_size, stringsAsFactors = FALSE)
+bg_param_setting <- cbind(bg_param_setting,
+                          data.frame(class = "MULTINOM", name = "Multinomial", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting$window_size_for_reg <- bg_param_setting$window_size
+d <- run_sim(bg_param_setting, additional_setting, microsoft_generated_data_2000,
+             list("max" = microsoft_generated_data_2000, "avg" = microsoft_generated_data_2000, "min" = microsoft_generated_data_2000, "sd" = microsoft_generated_data_2000, "median" = microsoft_generated_data_2000),
+             cores = parallel::detectCores(),
+             write_type = c("charwise", "paramwise"),
+             plot_type = "none",
+             result_loc = "~/SimulationResult/SmartHarvestComparison/Multinom/")
 
 
