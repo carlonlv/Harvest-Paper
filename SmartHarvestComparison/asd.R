725a65f80417c4d3f28330a76642b21e5336fbda
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-03-16 02:24:08 -0400

    Update asd.R

--- SmartHarvestComparison/asd.R
+++ SmartHarvestComparison/asd.R
@@ -2,24 +2,25 @@ library(DataCenterSim)
 library(dplyr)
 
 load("~/microsoft_generated_data_3000.rda")
-microsoft_generated_data_3000 <- microsoft_generated_data_3000[1:(3000 * 30),]
+microsoft_generated_data_3000 <- microsoft_generated_data_3000[1:(3000 * 30), ]
 
 window_size <- c(1, 5, 10, 15, 20, 25) * 30
 
 window_type_for_reg <- c("max", "avg", "min", "sd", "median")
 
 ## 8, 16, 32, 64 Cores
-granularity <- c(100 / 8, 100 / 16, 100 / 32, 100 / 64)
+granularity <- c(100 / 16, 100 / 32,  100 / 48, 100 / 64)
 
 cut_off_prob <- c(0.0001, 0.0003, 0.0005, 0.001, 0.003, 0.005, 0.01, 0.03, 0.05)
 additional_setting <- list("cut_off_prob" = cut_off_prob, "window_type_for_reg" = window_type_for_reg)
 
 bg_param_setting <- expand.grid("granularity" = granularity, "window_size" = window_size, stringsAsFactors = FALSE)
 bg_param_setting <- cbind(bg_param_setting,
-                          data.frame(class = "MULTINOM", name = "Multinomial", train_policy = "fixed", train_size = 2000 * 30, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+                          data.frame(class = "MULTINOM", name = "Multinomial", train_policy = "fixed", train_size = 1990 * 30, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 bg_param_setting$window_size_for_reg <- bg_param_setting$window_size
 d <- run_sim(bg_param_setting, additional_setting, microsoft_generated_data_3000,
              list("max" = microsoft_generated_data_3000, "avg" = microsoft_generated_data_3000, "min" = microsoft_generated_data_3000, "sd" = microsoft_generated_data_3000, "median" = microsoft_generated_data_3000),
+             start_point = 10 * 30 + 1,
              cores = parallel::detectCores(),
              write_type = c("charwise", "paramwise"),
              plot_type = "none",
