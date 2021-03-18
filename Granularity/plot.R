e72de1a51f5b75997a03bc48c055049ea0d6e2d2
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-03-18 18:13:33 -0400

    Update

--- Granularity/plot.R
+++ Granularity/plot.R
@@ -16,12 +16,12 @@ for (i in result_files) {
   a <- read.csv(i)
   a <- a[, c("name", "granularity", "window_size", "quantile", grep("score", colnames(a), value = TRUE))]
   a <- a[a$window_size == window_size,]
-  a <- a[a$name == "AR1",]
+  a <- a[a$name == name,]
   overall_df <- rbind(overall_df, a)
 }
 plot_sim_charwise(overall_df,
                   mapping = list("color" = "granularity"),
-                  adjusted = FALSE,
+                  adjusted = TRUE,
                   point_or_line = NA,
-                  name = paste0("Different Number of cores at Window Size of ", window_size, " with ", name),
+                  name = paste0("Adjusted Different Number of cores at Window Size of ", window_size, " with ", name),
                   path)
