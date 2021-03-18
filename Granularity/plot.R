5dc6d001bcccddbb48b93f7700537f0d6c1c33e8
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-03-18 14:01:00 -0400

    add plt

--- Granularity/plot.R
+++ Granularity/plot.R
@@ -1,25 +1,27 @@
 library(DataCenterSim)
 library(dplyr)
 
-path = "~/Documents/GitHub/SimulationResult/FindBestPredictor/"
+path = "~/Documents/GitHub/SimulationResult/Granularity/"
 
 result_files <- list.files(path, pattern = "Charwise*", full.names = TRUE, recursive = TRUE)
 
 ## Baseline max to max, same window sizes
 
 ### window_size of 1
-window_size <- 25
+window_size <- 20
+name <- "Markov"
 
 overall_df <- data.frame()
 for (i in result_files) {
   a <- read.csv(i)
-  a <- a[, c("name", "window_size", "quantile", grep("score", colnames(a), value = TRUE))]
+  a <- a[, c("name", "granularity", "window_size", "quantile", grep("score", colnames(a), value = TRUE))]
   a <- a[a$window_size == window_size,]
+  a <- a[a$name == "AR1",]
   overall_df <- rbind(overall_df, a)
 }
 plot_sim_charwise(overall_df,
-                  mapping = list("color" = "name"),
+                  mapping = list("color" = "granularity"),
                   adjusted = FALSE,
                   point_or_line = NA,
-                  name = paste0("Various Models with Different Regressors at Window Size of ", window_size),
+                  name = paste0("Different Number of cores at Window Size of ", window_size, " with ", name),
                   path)
