4271e8a98244e69bb27bf465f9d157fd874f1860
carlonlv <carlonlv@hotmail.com>
2021-03-28 22:17:21 -0400

    aasd

--- NN/asd.R
+++ NN/asd.R
@@ -9,6 +9,7 @@ microsoft_avg_10000 <- microsoft_avg_10000[1:3000, c(1:3019)[-c(286,290,328,380,
 cut_off_prob <- c(0.001, 0.003, 0.01, 0.03, 0.05)
 p <- c(1, 2, 3, NA_real_)
 window_size <- c(1, 10, 20, 30)
+additional_setting <- list("cut_off_prob" = cut_off_prob)
 bg_param_setting <- expand.grid(p = p, window_size = window_size, stringsAsFactors = FALSE)
 bg_param_setting <- cbind(bg_param_setting, data.frame(name = "NN", P = 0, granularity = 0, train_policy = "offline", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
-d <- run_sim(bg_param_setting, microsoft_max_10000, NULL, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/NN/p/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/NN/p/")
