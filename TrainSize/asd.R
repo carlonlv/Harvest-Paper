565a7cd07e541dd04944e3661fe9b2e6117979c4
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-03-30 13:04:18 -0400

    Update asd.R

--- TrainSize/asd.R
+++ TrainSize/asd.R
@@ -20,25 +20,25 @@ additional_setting <- list("cut_off_prob" = cut_off_prob)
 
 for (i in train_size) {
   
-  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, train_size = train_size, stringsAsFactors = FALSE)
+  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, stringsAsFactors = FALSE)
   bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1", granularity = granularity, train_size = i, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   bg_param_setting$window_size_for_xreg <- bg_param_setting$window_size
   bg_param_setting$window_type_for_xreg <- "avg"
   d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, start_point = 2000 - i + 1, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/TrainSize/AR1/")
   
-  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, train_size = train_size, stringsAsFactors = FALSE)
+  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, stringsAsFactors = FALSE)
   bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1X(avg)", granularity = granularity, train_size = i, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   d <- run_sim(bg_param_setting,additional_setting,  microsoft_max_10000, microsoft_avg_10000, start_point = 2000 - i + 1, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/TrainSize/AR1X/")
   
-  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, train_size = train_size, stringsAsFactors = FALSE)
+  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, stringsAsFactors = FALSE)
   bg_param_setting <- cbind(bg_param_setting, data.frame(name = "MARKOV", name = "Markov", granularity = granularity, train_size = i, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   d <- run_sim(bg_param_setting,additional_setting,  microsoft_max_10000, NULL, start_point = 2000 - i + 1, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/TrainSize/Markov/")
   
-  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, train_size = train_size, cluster_type = c("fixed", "quantile"), stringsAsFactors = FALSE)
+  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, cluster_type = c("fixed", "quantile"), stringsAsFactors = FALSE)
   bg_param_setting <- cbind(bg_param_setting, data.frame(name = "MARKOV", name = "Markov(quantile)", cluster_type = "quantile", granularity = granularity, train_size = i, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, start_point = 2000 - i + 1, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/TrainSize/MarkovX/")
   
-  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, train_size = train_size, stringsAsFactors = FALSE)
+  bg_param_setting <- expand.grid(window_size = window_size, train_policy = train_policy, stringsAsFactors = FALSE)
   bg_param_setting <- cbind(bg_param_setting, data.frame(class = "VAR", name = "VAR1", p = 1, granularity = granularity, train_size = i, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   bg_param_setting$window_size_for_xreg <- bg_param_setting$window_size
   bg_param_setting$window_type_for_xreg <- "avg"
