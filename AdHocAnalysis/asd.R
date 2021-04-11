7f477d04f07b15a0c04b05f218da8deb918bb971
carlonlv <carlonlv@hotmail.com>
2021-04-10 20:02:20 -0400

    ddd

--- AdHocAnalysis/asd.R
+++ AdHocAnalysis/asd.R
@@ -1,7 +1,7 @@
 library(DataCenterSim)
 library(dplyr)
 
-load("~/Documents/PDSF Dataset/microsoft_10000.rda")
+load("~/microsoft_10000.rda")
 
 microsoft_max_10000 <- microsoft_max_10000[1:3000, c(1:3019)[-c(286,290,328,380,387,398,399,704,706,718,720,738,813,1571,1637,1638,2021,3012,3018)]]
 microsoft_avg_10000 <- microsoft_avg_10000[1:3000, c(1:3019)[-c(286,290,328,380,387,398,399,704,706,718,720,738,813,1571,1637,1638,2021,3012,3018)]]
@@ -15,5 +15,5 @@ cut_off_prob <- c(0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05)
 additional_setting <- list("cut_off_prob" = cut_off_prob)
 
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1", res_dist = "discretized", extrap_step = 2, train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = 1, write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/AdHocAnalysis/AR1/")
