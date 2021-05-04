d387d811d46b78184cd7ed974c5b2a2675b1d121
Jialun Lyu <jialun@swift-073.syslab.sandbox>
2021-05-04 09:51:08 -0400

    asddd

--- MultipleorSmallerJobs/asd.R
+++ MultipleorSmallerJobs/asd.R
@@ -9,7 +9,7 @@ microsoft_avg_10000 <- microsoft_avg_10000[1:3000, c(1:3019)[-c(286,290,328,380,
 granularity <- c(100 / 32)
 ## add 100 / 64
 
-schedule_setting <- c("2_jobs", "4_jobs", "1_cores", "2_cores")
+schedule_setting <- c("2_cores")
 window_size <- c(1)
 ## add 1
 
@@ -19,4 +19,4 @@ additional_setting <- list("cut_off_prob" = cut_off_prob)
 
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, schedule_setting = schedule_setting, stringsAsFactors = FALSE)
 bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1", extrap_step = 1, train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = parallel::detectCores() - 1, write_type = c("paramwise", "charwise"), plot_type = "none", result_loc = "~/MultipleorSmallerJobs/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = parallel::detectCores(), write_type = c("paramwise", "charwise"), plot_type = "none", result_loc = "~/MultipleorSmallerJobs/")
