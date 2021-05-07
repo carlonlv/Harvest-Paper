322f4dd7f90dcb4db2f2fabb32c64ca75a0350b7
Jialun Lyu <jialun@mel-16.syslab.sandbox>
2021-05-07 10:19:35 -0400

    asdd

--- MultipleorSmallerJobs/asd2.R
+++ MultipleorSmallerJobs/asd2.R
@@ -4,7 +4,7 @@ library(dplyr)
 load("~/microsoft_generated_data_3000.rda")
 microsoft_generated_data_3000 <- microsoft_generated_data_3000[1:(3000 * 30), ]
 
-window_size <- c(1, 5, 10, 15, 20, 25)
+window_size <- c(10, 20, 30, 40, 50)
 
 granularity <- c(100 / 32)
 
@@ -12,7 +12,7 @@ cut_off_prob <- c(0.0001, 0.0003, 0.0005, 0.001, 0.003, 0.005, 0.01, 0.03, 0.05)
 
 schedule_setting <- c("max_size", "2_jobs", "4_jobs", "1_cores", "2_cores")
 
-additional_setting <- list("cut_off_prob" = cut_off_prob, "window_type_for_reg" =  c("max", "avg", "min", "sd", "median"), "window_size_for_reg" = 1)
+additional_setting <- list("cut_off_prob" = cut_off_prob, "window_type_for_reg" =  c("max", "avg", "min", "sd", "median"), "include_past_window_size" = T)
 
 temp_x <- do.call(cbind, parallel::mclapply(1:ncol(microsoft_generated_data_3000), function(k) {
   convert_frequency_dataset(microsoft_generated_data_3000[,k], 30, "max")
@@ -26,10 +26,10 @@ for (i in window_size) {
                             data.frame(class = "MULTINOM", name = "Multinomial(max.avg.min.sd.median)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
   temp_xreg <- lapply(additional_setting[["window_type_for_reg"]], function(j) {
     asd <- do.call(cbind, parallel::mclapply(1:ncol(microsoft_generated_data_3000), function(k) {
-      convert_frequency_dataset(microsoft_generated_data_3000[,k], 30 * i, j)
-    }, mc.cores = parallel::detectCores))
+      convert_frequency_dataset(microsoft_generated_data_3000[,k], 30, j)
+    }, mc.cores = parallel::detectCores()))
     colnames(asd) <- colnames(temp_x)
-    rownames(asd) <- rownames(temp_x)
+    rownames(asd) <- 1:nrow(asd)
     return(asd)
   })
 
