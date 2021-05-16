67006825a67edb29eaf04058c89e2b1175e455f5
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-05-16 18:15:59 -0400

    Update

--- FindBestPredictor/asd4.R
+++ FindBestPredictor/asd4.R
@@ -16,13 +16,13 @@ bg_param_setting <- expand.grid(p = p, window_size = window_size, stringsAsFacto
 bg_param_setting <- cbind(bg_param_setting, data.frame(class = "VAR", granularity = granularity, train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 bg_param_setting$window_size_for_xreg <- bg_param_setting$window_size
 
-additional_setting <- list("cut_off_prob" = cut_off_prob, "window_type_for_reg" = window_type_for_reg)
+additional_setting <- list("cut_off_prob" = cut_off_prob, "include_response_window_size" = TRUE, "window_type_for_reg" = window_type_for_reg)
 
 bg_param_setting <- cbind(bg_param_setting,
                           data.frame(class = "VAR", name = "VAR1(max,avg,min,sd,median)", granularity = granularity, train_policy = "fixed", train_size = 1990 * 30, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 bg_param_setting$window_size_for_reg <- bg_param_setting$window_size
 d <- run_sim(bg_param_setting, additional_setting, microsoft_generated_data_3000,
-             list("max" = microsoft_generated_data_3000, "avg" = microsoft_generated_data_3000, "min" = microsoft_generated_data_3000, "sd" = microsoft_generated_data_3000, "median" = microsoft_generated_data_3000),
+             microsoft_generated_data_3000,
              start_point = 10 * 30 + 1,
              cores = parallel::detectCores(),
              write_type = c("charwise", "paramwise"),
