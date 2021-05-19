f7e4e7743ed65ab1cbd876f67d68745b7d346a2c
carlonlv <carlonlv@hotmail.com>
2021-05-19 09:47:59 -0400

    asddd

--- StateNum/asd2.R
+++ StateNum/asd2.R
@@ -1,7 +1,7 @@
 library(DataCenterSim)
 library(dplyr)
 
-load("~/microsoft_10000.rda")
+load("~/Documents/PDSF Dataset/microsoft_10000.rda")
 
 microsoft_max_10000 <- microsoft_max_10000[1:3000, c(1:3019)[-c(286,290,328,380,387,398,399,704,706,718,720,738,813,1571,1637,1638,2021,3012,3018)]]
 microsoft_avg_10000 <- microsoft_avg_10000[1:3000, c(1:3019)[-c(286,290,328,380,387,398,399,704,706,718,720,738,813,1571,1637,1638,2021,3012,3018)]]
@@ -18,4 +18,4 @@ additional_setting <- list("cut_off_prob" = cut_off_prob)
 
 bg_param_setting <- expand.grid(granularity = granularity, state_num = state_num, window_size = window_size, stringsAsFactors = FALSE)
 bg_param_setting <- cbind(bg_param_setting, data.frame(class = "MARKOV", name = "Markov(quantile)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, cluster_type = "quantile", stringsAsFactors = FALSE))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/StateNum/Markov/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, NULL, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/Documents/SimulationResult/StateNum/Markov/")
