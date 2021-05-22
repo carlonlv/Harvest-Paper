72336b6b16b256dddcf7357a70a5da83d00cf311
carlonlv <36804238+carlonlv@users.noreply.github.com>
2021-05-22 11:40:11 -0400

    asdd

--- ARIMAFamily/asd2.R
+++ ARIMAFamily/asd2.R
@@ -16,35 +16,35 @@ additional_setting <- list("cut_off_prob" = cut_off_prob, "include_response_wind
 
 ## AR1
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR1", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARX1", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 additional_setting$train_args <- list("order" = c(1,0,0))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
 
 ## AR2
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "AR2", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARX2", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 additional_setting$train_args <- list("order" = c(2,0,0))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
 
 ## ARMA (1,1)
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARMA(1.1)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARMAX(1.1)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 additional_setting$train_args <- list("order" = c(1,0,1), "method" = "ML", "optim.method" = "BFGS")
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
 
 ## MA1
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "MA1", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "MAX1", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 additional_setting$train_args <- list("order" = c(0,0,1))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
 
 ## ARIMA (1,1,1)
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARIMA(1.1.1)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "ARIMA", name = "ARIMAX(1.1.1)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
 additional_setting$train_args <- list("order" = c(1,1,1))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
 
 ## AutoARIMA
 bg_param_setting <- expand.grid(granularity = granularity, window_size = window_size, stringsAsFactors = FALSE)
-bg_param_setting <- cbind(bg_param_setting, data.frame(class = "AUTO_ARIMA", name = "AUTOARIMA", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
-d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/AR1/")
+bg_param_setting <- cbind(bg_param_setting, data.frame(class = "AUTO_ARIMA", name = "ARIMAX(auto)", train_policy = "fixed", train_size = 2000, update_freq = 3, react_speed = "1,2", extrap_step = 1, stringsAsFactors = FALSE))
+d <- run_sim(bg_param_setting, additional_setting, microsoft_max_10000, microsoft_avg_10000, cores = parallel::detectCores(), write_type = c("charwise", "paramwise"), plot_type = "none", result_loc = "~/SimulationResult/ARIMAFamily/")
