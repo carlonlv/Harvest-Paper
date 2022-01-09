bf17b391d4d9e51f3b599ff928e4fd17bebd8886
carlonlv <36804238+carlonlv@users.noreply.github.com>
2022-01-08 23:55:44 -0500

    UPdate

--- DataGrepper/postprocessing.py
+++ DataGrepper/postprocessing.py
@@ -9,10 +9,8 @@ import os
 import pickle
 import time
 from multiprocessing import Manager
-from os import path
 
 import pandas as pd
-from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
 
@@ -20,29 +18,24 @@ def apply_filter_production(df):
     """
     Apply filter for Production jobs.
     """
-    return df.groupby("collection_id").filter(lambda x: all(x["priority"] >= 120) and all(x["type"] not in [4, 5, 6, 7, 8]))
+    return df.groupby("collection_id").filter(lambda x: (x["priority"] >= 120).all() and not x["type"].isin([4, 5, 6, 7, 8]).any())
 
 def apply_filter_batch(df):
     """
     Apply filter for Batch jobs.
     """
-    return df.groupby("collection_id").filter(lambda x: all(x["priority"] <= 119) and all(x["type"] not in [4, 5, 7, 8]))
-
-def normalize(line):
-    """
-    Convert json to pd.DataFrame
-    """
-    return pd.json_normalize(json.loads(line), max_level=2)
+    return df.groupby("collection_id").filter(lambda x: (x["priority"] <= 119).all() and not x["type"].isin([4, 5, 7, 8]).any())
 
 def convert_to_df(lines):
     """
     Convert lines to DataFrame.
     """
     temp_df = []
-    for line in tqdm(lines):
-        temp_df.extend(list(normalize(line)))
-    temp_df = pd.concat(temp_df, sort = False)
+    for line in lines:
+        temp_df.append(json.loads(line))
+    temp_df = pd.json_normalize(temp_df, max_level=3)
     temp_df["collection_id"] = temp_df["collection_id"].astype(int)
+    temp_df["instance_index"] = temp_df["instance_index"].astype(int)
     temp_df["priority"] = temp_df["priority"].astype(int)
     temp_df["type"] = temp_df["type"].astype(int)
     temp_df["collection_type"] = temp_df["collection_type"].astype(int)
@@ -57,11 +50,11 @@ def write_df(df, target_fp):
     else:
         df.to_csv(target_fp, header = True)
 
-def process(file_name):
+def process_job_events(file_name):
     """
     Process collection_events files.
     """
-    r = gzip.open(path + 'job_events' + '/' + file_name, 'rt')
+    r = gzip.open(PATH + "job_events/" + file_name, 'rt')
     r.seek(0, 0)
     while True:
         l = r.readlines(MAX_READ_SIZE)
@@ -77,39 +70,103 @@ def process(file_name):
         selected_batch_job_ids.extend((batch_df.loc[:,"collection_id"]).tolist())
         selected_production_job_ids.extend((production_df.loc[:,"collection_id"]).tolist())
 
+def process_task_events(file_name):
+    """
+    Process instance_events files.
+    """
+    r = gzip.open(PATH + "task_events/" + file_name, 'rt')
+    r.seek(0, 0)
+    while True:
+        l = r.readlines(MAX_READ_SIZE)
+        if not l:
+            break
+        tt_df = convert_to_df(l)
+        batch_df = tt_df.loc[tt_df["collection_id"].isin(selected_batch_job_ids)]
+        production_df = tt_df.loc[tt_df["collection_id"].isin(selected_production_job_ids)]
+
+        write_df(batch_df, PATH + "parsed_task_events/" + TARGET_BATCH_TASK_FILE_NAME)
+        write_df(production_df, PATH + "parsed_task_events/" + TARGET_PRODUCTION_TASK_FILE_NAME)
+
+def process_task_usage(file_name):
+    """
+    Process and Consume task usage files.
+    """
+    r = gzip.open(PATH + "task_usage/" + file_name, 'rt')
+    r.seek(0, 0)
+    while True:
+        l = r.readlines(MAX_READ_SIZE)
+        if not l:
+            break
+        tt_df = convert_to_df(l)
+        batch_df = tt_df.loc[tt_df["collection_id"].isin(selected_batch_job_ids) & tt_df["instance_index"] == 0]
+        production_df = tt_df.loc[tt_df["collection_id"].isin(selected_production_job_ids) & tt_df["instance_index"] == 0]
+
+        write_df(batch_df, PATH + "parsed_task_usage/" + TARGET_BATCH_TASK_USAGE_FILE_NAME)
+        write_df(production_df, PATH + "parsed_task_usage/" + TARGET_PRODUCTION_TASK_USAGE_FILE_NAME)
+    os.remove(file_name)
+
 if __name__ == "__main__":
     HEAD_PATH = "/mnt/scratch/"
     PATH = HEAD_PATH + "google_2019_data/"
-    CORES = 1
 
-    MAX_READ_SIZE = 1024
+    CORES = 28
 
-    st = time.time()
-    job_events = sorted(os.listdir(path + "job_events"))
+    MAX_READ_SIZE = 1 * (1024 ** 3)
 
     TARGET_BATCH_JOB_FILE_NAME = "job_events_batch_df.csv"
     TARGET_PRODUCTION_JOB_FILE_NAME = "job_events_production_df.csv"
 
+    TARGET_BATCH_TASK_FILE_NAME = "task_events_batch_df.csv"
+    TARGET_PRODUCTION_TASK_FILE_NAME = "task_events_production_df.csv"
+
+    TARGET_BATCH_TASK_USAGE_FILE_NAME = "task_usage_batch_df.csv"
+    TARGET_PRODUCTION_TASK_USAGE_FILE_NAME = "task_usage_production_df.csv"
+
     TARGET_BATCH_SELECTED_JOB_FILE_NAME = "selected_job_events_batch.pkl"
     TARGET_PRODUCTION_SELECTED_JOB_FILE_NAME = "selected_job_events_production.pkl"
+
+    ## Section 1: Job Events
+    st = time.time()
+    job_events = sorted(os.listdir(PATH + "job_events"))
 
     with Manager() as manager:
         selected_batch_job_ids = manager.list()
         selected_production_job_ids = manager.list()
-        process_map(process, job_events, max_workers = CORES)
+        process_map(process_job_events, job_events, max_workers = CORES)
 
-    selected_batch_job_ids = set(selected_batch_job_ids)
-    selected_production_job_ids = set(selected_production_job_ids)
+        selected_batch_job_ids = set(selected_batch_job_ids)
+        selected_production_job_ids = set(selected_production_job_ids)
 
-    print("Total Number of Batch Jobs is " + len(selected_batch_job_ids))
-    print("Total Number of Production Jobs is " + len(selected_production_job_ids))
+        print("Total Number of Batch Jobs is " + str(len(selected_batch_job_ids)))
+        print("Total Number of Production Jobs is " + str(len(selected_production_job_ids)))
 
-    with open(path + TARGET_BATCH_SELECTED_JOB_FILE_NAME, "wb") as r:
-        pickle.dump(selected_batch_job_ids, r, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(PATH + TARGET_BATCH_SELECTED_JOB_FILE_NAME, "wb") as f:
+            pickle.dump(selected_batch_job_ids, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-    with open(path + TARGET_PRODUCTION_SELECTED_JOB_FILE_NAME, "wb") as r:
-        pickle.dump(selected_production_job_ids, r, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(PATH + TARGET_PRODUCTION_SELECTED_JOB_FILE_NAME, "wb") as f:
+            pickle.dump(selected_production_job_ids, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     et = time.time()
-    print("Filtering Job Ids took" , (et - st) / 60," minutes.")
-    
+    print("Processing Job Events took" , (et - st) / 60," minutes.")
+
+    ## Section 2: Task Events
+    with open(PATH + TARGET_BATCH_SELECTED_JOB_FILE_NAME, "rb") as f:
+        selected_batch_job_ids = pickle.load(f)
+    with open(PATH + TARGET_PRODUCTION_SELECTED_JOB_FILE_NAME, "rb") as f:
+        selected_production_job_ids = pickle.load(f)
+    st = time.time()
+    task_events = sorted(os.listdir(PATH + "task_events"))
+    process_map(process_task_events, task_events, max_workers = CORES)
+    et = time.time()
+    print("Processing Task Events took" , (et - st) / 60," minutes.")
+
+    ## Section 3: Task Usage
+    with open(PATH + TARGET_BATCH_SELECTED_JOB_FILE_NAME, "rb") as f:
+        selected_batch_job_ids = pickle.load(f)
+    with open(PATH + TARGET_PRODUCTION_SELECTED_JOB_FILE_NAME, "rb") as f:
+        selected_production_job_ids = pickle.load(f)
+    st = time.time()
+    task_usage = sorted(os.listdir(PATH + "task_usage"))
+    process_map(process_task_usage, task_usage, max_workers = CORES)
+    et = time.time()
+    print("Processing Task Usage took" , (et - st) / 60," minutes.")
